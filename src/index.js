528f4019ad81dc1e8a0e4f2523abe91c2b4d037a
bakliwalkshitiz <kshitizbakliwal72@gmail.com>
2025-01-16 17:12:48 +0530

    completed city repository

--- src/index.js
+++ src/index.js
@@ -2,18 +2,18 @@ const express = require("express");
 const bodyParser = require("body-parser"); 
 
 const { PORT } = require('./config/ServerConfig');
-
+const CityRepository = require('./repository/city-repository');
 
 const setupAndStartServer = async () => {
     // Create the express object
     const app = express();
-   
+    
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}));
 
-
-    app.listen(PORT, () => {
+    app.listen(PORT,  async () => {
         console.log(`Server Started at ${PORT}`);
+       
     });
 };
 
--- src/migrations/20250107161914-create-city.js
+++ src/migrations/20250107161914-create-city.js
@@ -10,7 +10,9 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       name: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false , // changes done ie -> added by me 
+        unique: true
       },
       createdAt: {
         allowNull: false,
